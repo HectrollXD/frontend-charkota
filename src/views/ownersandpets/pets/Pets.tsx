1089ab855fc5a1bc29900190d7ae0225053a57f8
Hector Rodriguez <hector.rmtnez89@gmail.com>
2023-12-05 00:39:52 -0600

    Se agrega tabla para mostrar mascotas

--- src/views/ownersandpets/pets/Pets.tsx
+++ src/views/ownersandpets/pets/Pets.tsx
@@ -1,29 +1,115 @@
 //-------------------------------------------------------------------------------------------------- Imports
-
-import { Button } from "antd";
+import { Button, Col, Row, Table, notification } from "antd";
 import DefaultLayout from "../../../components/DefaultLayout";
 import StyleSheet from "../../../StyleSheet";
 import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { ColumnsType } from "antd/es/table";
+import _ from "lodash";
+import { getAllPets } from "../../../methdos/methdos";
+
+
+
+//-------------------------------------------------------------------------------------------------- Interfaces
+export interface PetDataType {
+	key: string;
+	name: string;
+	animalBreedAndFamily: string;
+	owner: string;
+	vaccines: string;
+};
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
+
+
 
 //-------------------------------------------------------------------------------------------------- View
 const Pets = () => {
 	const navigate = useNavigate();
+	// Control
+	const [api, contextHolder] = notification.useNotification();
+	// Data
+	const [pets, setPets] = useState<PetDataType[]>([]);
+	const columns: ColumnsType<PetDataType> = [
+		{
+			title: 'Nombre de mascota',
+			dataIndex: 'name',
+		},
+		{
+			title: 'Raza y familia de animal',
+			dataIndex: 'animalBreedAndFamily',
+		},
+		{
+			title: 'Nombre del propietario',
+			dataIndex: 'owner',
+		},
+		{
+			title: 'Vacunas',
+			dataIndex: 'vaccines',
+		}
+	];
 
 
 
-	return(
-		<DefaultLayout
-			title="Mascotas"
-			renderButton={
-				<Button style={styles.createNewButton} type="primary" onClick={() => navigate("create-new")}>
-					Agregar mascotas
-				</Button>
+	const openNotification = useCallback((
+		type: NotificationType, message: string
+	) => {
+		api[type]({
+			type: type,
+			message: "Mascotas",
+			description: message,
+			duration: 5
+		});
+	}, [api]);
+
+
+
+	useEffect(() => {
+		getAllPets().then((resp) => {
+			if (resp.status) {
+				console.log(resp.data.pets);
+				setPets(
+					resp.data.pets.map(pet => ({
+						key: pet.id,
+						name: pet.name,
+						animalBreedAndFamily: `${pet.animalBreed.breedName} | ${pet.animalBreed.family.name}`,
+						owner: `${pet.owner.name} ${pet.owner.lastname}`,
+						vaccines: pet.vaccines.map(vcc => vcc.vaccineName).toString()
+					} as PetDataType))
+				);
 			}
-		>
-			<h1>
-				Mascotas
-			</h1>
-		</DefaultLayout>
+			else {
+				openNotification("warning", resp.message);
+			}
+		}).catch(() => {
+			openNotification("error", "No fue posible obtener a las mascotas. Intente de nuevo más tarde.");
+		});
+	}, []);
+
+
+
+	return (
+		<>
+			{contextHolder}
+
+			<DefaultLayout
+				title="Mascotas"
+				renderButton={
+					<Button style={styles.createNewButton} type="primary" onClick={() => navigate("create-new")}>
+						Agregar mascota
+					</Button>
+				}
+			>
+				<Row>
+					<Col span={24}>
+						<Table
+							columns={columns}
+							dataSource={pets}
+						/>
+					</Col>
+				</Row>
+			</DefaultLayout>
+		</>
 	);
 };
 
